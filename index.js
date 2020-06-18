821a2ed1b182ab772359a7d915834d9111d1c593
kantam5 <59176502+kantam5@users.noreply.github.com>
2020-06-18 23:25:29 +0900

    #2.4 Handling Routes with Express

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ const handleListening = () => {
   console.log("Listening on: http://localhost:4000");
 };
 
+const handleHome = (req, res) => {
+  res.send("Hello from Home!");
+};
+
+const handleProfile = (req, res) => {
+  res.send("You are on my Profile");
+};
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
