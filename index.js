ae65c628a0162261e809d8828d666d14d9460a06
kantam5 <59176502+kantam5@users.noreply.github.com>
2020-06-19 01:17:30 +0900

    #2.5 ES6 on NodeJS using Babel

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
 const handleListening = () => {
-  console.log("Listening on: http://localhost:4000");
+  console.log(`Listening on: http://localhost:${PORT}`);
 };
 
 const handleHome = (req, res) => {
